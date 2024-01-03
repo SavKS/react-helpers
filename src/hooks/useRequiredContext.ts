53960b099cae4e50000baaec838dd1b1b131798e
Andrii Savluk <a.savluk@ideil.com>
2024-01-03 16:57:18 +0200

    v1.3.0

--- src/hooks/useRequiredContext.ts
+++ src/hooks/useRequiredContext.ts
@@ -7,7 +7,7 @@ function useRequiredContext<T>(context: Context<T | null>, name?: string): T;
 function useRequiredContext<T>(context: Context<T | undefined>, name?: string) {
     const payload = useContext(context);
 
-    if (!payload) {
+    if (payload === undefined) {
         const displayName = name ?? context.displayName;
 
         throw new Error(`Required${ displayName ? ` "${ displayName }" ` : '' }context not defined`);
