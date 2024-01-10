d1744841059182c4f7979e532cf7665a38431da4
Andrii Savluk <a.savluk@ideil.com>
2024-01-10 15:12:01 +0200

    v1.3.3

--- src/hooks/useRequiredContext.ts
+++ src/hooks/useRequiredContext.ts
@@ -2,12 +2,12 @@ import { Context, useContext } from 'react';
 
 function useRequiredContext<T>(context: Context<T | undefined>, name?: string): T;
 
-function useRequiredContext<T>(context: Context<T>, name?: string): T;
+function useRequiredContext<T>(context: Context<T | null>, name?: string): T;
 
 function useRequiredContext<T>(context: Context<T | undefined>, name?: string) {
     const payload = useContext(context);
 
-    if (payload === undefined) {
+    if (payload === undefined || payload === null) {
         const displayName = name ?? context.displayName;
 
         throw new Error(`Required${ displayName ? ` "${ displayName }" ` : '' }context not defined`);
